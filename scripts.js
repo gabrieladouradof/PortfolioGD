020d860435a1b7a48a982aa82750f120a2628d46
Gabriela Dourado <gdourado.066@gmail.com>
2024-09-09 22:54:49 -0300

    new project

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const carousel = document.querySelector('.carousel');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+carousel.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 2; // Velocidade de rolagem
+  carousel.scrollLeft = scrollLeft - walk;
+});
